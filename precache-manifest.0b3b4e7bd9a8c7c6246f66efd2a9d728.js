9c47be04938a8935b49ed56c7278450e44332997
klyswer <klyswer@gmail.com>
2020-05-06 16:21:57 -0400

    Updates

---
+++ precache-manifest.0b3b4e7bd9a8c7c6246f66efd2a9d728.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a966d6cd7abe7f48e268f1234e643e69",
+    "url": "/klyswer/repoPrueba.git/index.html"
+  },
+  {
+    "revision": "9387af357b0ee04a2ac4",
+    "url": "/klyswer/repoPrueba.git/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "259d9eaaf04833d90dca",
+    "url": "/klyswer/repoPrueba.git/static/js/2.4df710b4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/klyswer/repoPrueba.git/static/js/2.4df710b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9387af357b0ee04a2ac4",
+    "url": "/klyswer/repoPrueba.git/static/js/main.f83618ee.chunk.js"
+  },
+  {
+    "revision": "c3def00e78b4e05ca3a6",
+    "url": "/klyswer/repoPrueba.git/static/js/runtime-main.aec13f13.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/klyswer/repoPrueba.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
